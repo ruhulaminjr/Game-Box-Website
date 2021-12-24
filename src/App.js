691cf95aac31d47b8976754f20e47b679c00afca
ruhulaminjr <ruhultuber@gmail.com>
2021-12-24 17:11:50 +0600

    admin dashborad

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import AdminRoute from "./components/Pages/AdminRoute/AdminRoute";
+import MakeAdmin from "./components/Pages/Dashboard/AdminDashboard/MakeAdmin/MakeAdmin";
+import ManageOrders from "./components/Pages/Dashboard/AdminDashboard/ManageOrders/ManageOrders";
 import GameDetails from "./components/Pages/GameDetails/GameDetails";
 import Home from "./components/Pages/Home/Home/Home";
 import Register from "./components/Pages/Register/Register";
@@ -16,6 +19,26 @@ function App() {
             element={
               <PrivateRoute>
                 <GameDetails />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/makeadmin"
+            element={
+              <PrivateRoute>
+                <AdminRoute>
+                  <MakeAdmin />
+                </AdminRoute>
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/manageorders"
+            element={
+              <PrivateRoute>
+                <AdminRoute>
+                  <ManageOrders />
+                </AdminRoute>
               </PrivateRoute>
             }
           />
---
+++ src/components/Pages/Dashboard/AdminDashboard/MakeAdmin/MakeAdmin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MakeAdmin = () => {
+  return (
+    <div>
+      <h1>Make Admin this is </h1>
+    </div>
+  );
+};
+
+export default MakeAdmin;
